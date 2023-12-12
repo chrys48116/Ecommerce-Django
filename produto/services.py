9e5d1070ee797a8dbe0415239dd5ab3bd5344ec7
chrystian almeida <chrys481@gmail.com>
2023-12-12 12:32:14 -0300

    atualizando api de frete

--- produto/services.py
+++ produto/services.py
@@ -1,8 +1,13 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
 
 class CalculoFreteService:
     def calcular_frete(self, cep_destino, cep_origem=71591335, peso=1000, altura=50, largura=40, profundidade=30):
-        url = f'https://www.cepcerto.com/ws/json-frete/{cep_origem}/{cep_destino}/{peso}/{altura}/{largura}/{profundidade}/eafa7a13ac8c22a4bfdadc771d71a8015157e84f/'
+        url = f'https://www.cepcerto.com/ws/json-frete/{cep_origem}/{cep_destino}/{peso}/{altura}/{largura}/{profundidade}/{api_key}/'
         response = requests.get(url)
         return json.loads(response.text)
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,10 @@
 from perfil.models import Perfil
 import requests
-import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
 
 def format_preco(val):
     return f'R$ {val:.2f}'.replace('.',',')
@@ -37,6 +41,6 @@ def calcular_frete(request):
         return {'frete': {'error': str(e)}}
     
 def calcula_frete_service(cep_destino, cep_origem=71591335, peso=1000, altura=50, largura=40, profundidade=30):
-        url = f'https://www.cepcerto.com/ws/json-frete/{cep_origem}/{cep_destino}/{peso}/{altura}/{largura}/{profundidade}/eafa7a13ac8c22a4bfdadc771d71a8015157e84f/'
+        url = f'https://www.cepcerto.com/ws/json-frete/{cep_origem}/{cep_destino}/{peso}/{altura}/{largura}/{profundidade}/{api_key}/'
         response = requests.get(url)
         return json.loads(response.text)
